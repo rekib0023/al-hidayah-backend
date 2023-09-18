da32a200dbc893d40cb026e0f57ab9c336e46441
Rekib Ahmed <rkb.ra0025@gmail.com>
2023-09-19 00:34:55 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -24,7 +24,9 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: (origin, callback) => {
+      callback(null, true);
+    },
     credentials: true,
   })
 );
